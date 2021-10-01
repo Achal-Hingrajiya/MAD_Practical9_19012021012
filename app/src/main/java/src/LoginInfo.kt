ba3de0e270259935b13c87e910f4de4516ef4281
Achal-H14 <apcomp1410@gmail.com>
2021-10-02 01:46:00 +0530

    Added BottomnavigationBar to dashboard Activity

--- app/src/main/java/src/LoginInfo.kt
+++ app/src/main/java/src/LoginInfo.kt
@@ -3,17 +3,24 @@ package src
 class LoginInfo {
     companion object{
 
-        lateinit var full_name : String;
-        lateinit var  phone_number : String;
-        lateinit var city: String;
-        lateinit var email : String;
-        lateinit var password: String;
-        var logged_in : Boolean = false;
-        var signed_up : Boolean = false
+//        lateinit var full_name : String;
+//        lateinit var  phone_number : String;
+//        lateinit var city: String;
+//        lateinit var email : String;
+//        lateinit var password: String;
+
+         var full_name : String = "Achal Hingrajiya"
+         var  phone_number : String = "+91 6355689874"
+         var city: String = "Rajkot, Gujarat"
+         var email : String = "achalhingrajiya19@gnu.ac.in"
+         var password: String = "abcd"
+        var logged_in : Boolean= true;
 
 
         fun registered () : Boolean{
-            return this :: email.isInitialized
+//            return this :: email.isInitialized
+            return true
+
         }
         fun signUp(full_name : String, phone_number : String, city : String, email : String, password : String){
             this.full_name = full_name
